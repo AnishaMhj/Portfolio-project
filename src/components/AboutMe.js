df448977854dac0e08d22a4c0f1abbafd2634a5d
anishmhrzn47 <anishamhrzn47@gmail.com>
2021-04-11 13:30:14 +1000

    added about-me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,10 +6,12 @@ const AboutMe = () => {
         <div className="container py-5"> 
             <div className="row">
                 <div className="col-lg-6 col-xm-12 ">
-                    <img src={author} alt="author.." />
+                    <div className="photo-wrap mb-5">
+                    <img className="profileImg" src={author} alt="author.." />
+                    </div>
                 </div>
                 <div className="col-lg-6 col-xm-12 ">
-                    <h1>About me</h1>
+                    <h1 className="about-heading">About me</h1>
                     <p>Hello!!</p>
                     <p>Lorem Ipsum is simply dummy text of the printing and 
                         typesetting industry. Lorem Ipsum has been the industry's 
